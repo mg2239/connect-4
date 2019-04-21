b3f131d67f66a840386b4a01cf7700c7de2c60d6
Imaan S. I. Rahim <ii53@cornell.edu>
2019-04-21 13:45:12 -0400

    Added type for minmaxtree

--- aiplayer.ml
+++ aiplayer.ml
@@ -1,10 +1,19 @@
 open Board
+open State
 
 let depth = 3
 
-(** Creates a new minmax tree based on the current board state, takes
-    into account whether a column is full. Stops tree if column is full. *)
-let generate_minmax_tree st = failwith ""
+(**The minmax tree of possible moves is represented as a 7-ary tree where
+   the root node is the current state and the children of a node are the states
+   resulting from possible moves. A leaf in the tree is represented by a node
+   whose list of child nodes is empty. Scores are initially only evaluated at
+   a leaf. Score values at all other nodes are set to 0.
+   A Node has attributes score:int, state: State.t, next_move:int, 
+   children:minmaxtree list (of maximum size 7)
+*)
+type minmaxtree = Node of int*State.t*int*(minmaxtree list)
+
+
 
 let score board =
   let check_rows grid = 
@@ -72,6 +81,12 @@ let score board =
     loopcols 0 0
   in
   check_subgrids
+
+
+(** Creates a new minmax tree based on the current board state, takes
+    into account whether a column is full. Stops tree if column is full. *)
+let generate_minmax_tree st = 
+  failwith ""
 
 (** Takes fully evaluated tree with scores at Leafs (But not at nodes), 
     returns the next move for the ai to make *)
