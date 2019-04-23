39efb2fdaeac19c59b0602e2874f4591e8fe931c
Imaan S. I. Rahim <ii53@cornell.edu>
2019-04-23 14:48:29 -0400

    Finished minmax algo, need to test

--- aiplayer.ml
+++ aiplayer.ml
@@ -23,23 +23,56 @@ let generate_minmax_tree st =
       if count = 7 then []
       else 
         match go count curr_state with
-        |Legal res -> Node (0, res, count, gen_children res (d+1))::(loop_norm (count+1))
+        |Legal res -> 
+          if check_win (board res)=None then
+            Node (-100, res, count, gen_children res (d+1))
+            ::(loop_norm (count+1))
+          else Node (100, res, count, [])::(loop_norm (count+1))
         |Illegal -> (loop_norm (count + 1)) in 
     let rec loop_leaf count = 
       if count = 7 then []
       else 
         match go count curr_state with
         |Legal res -> 
-          Node (Board.score (board res), res, count, gen_children res (d+1))::(loop_norm (count+1))
+          if check_win (board res)=None then
+            Node (Board.score (board res), res, count, gen_children res (d+1))
+            ::(loop_norm (count+1))
+          else Node (100, res, count, [])::(loop_norm (count+1))
         |Illegal -> (loop_norm (count + 1)) in 
     if d = depth then loop_leaf 0(*create the leaf layer *)
     else loop_norm 0 in
   (*loop over 7 Nodes *)
   (*call gen_Children on the root *)
-  Node (0, st, -1, gen_children st 0)
+  Node (-100, st, -1, gen_children st 0)
+
 (** Takes fully evaluated tree with scores at Leafs (But not at nodes), 
     returns the next move for the ai to make *)
-let eval_tree t = failwith ""
+let eval_tree t = 
+
+  let rec get_score children curr_score =
+    match children with 
+    |[]-> curr_score
+    |Node (sc, st, m, c)::t -> 
+      if State.current_player st = B then 
+        max (get_score c sc) (get_score t curr_score)
+      else 
+        min (get_score c sc) (get_score t curr_score) in
+
+  let scored_children root_children = List.map 
+      (fun (Node (sc, st, m, c)) -> Node (get_score c sc, st, m, c)) 
+      root_children in
+
+  let extract_best_scoring_move scored_root_children =
+    List.fold_left 
+      (fun (best_sc, best_move) (Node (sc, st, move, c))->
+         (if sc>best_sc then (sc, move)
+          else (best_sc, best_move))
+      ) (-100, -1) scored_root_children in
+
+  let Node (sc, state, move, children) = t in
+
+  extract_best_scoring_move (scored_children (children))
+
 
 (** returns a state after the AI makes a move *)
 let make_move_ai st = failwith ""
